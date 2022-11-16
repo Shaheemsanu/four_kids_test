01f5fe384eb577ec6557402f3f9d26cd8fc9cccc
shaheemsanu <shaheemsanu12@gmail.com>
2022-11-16 14:15:07 +0530

    on

--- lib/src/presentation/view/onboarding_screen/widgets/ononboarding_footer_row.dart
+++ lib/src/presentation/view/onboarding_screen/widgets/ononboarding_footer_row.dart
@@ -1,4 +1,4 @@
-/* import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/container.dart';
 import 'package:flutter/src/widgets/framework.dart';
 
 class OnonboardingFooterRow extends StatelessWidget {
@@ -8,5 +8,5 @@ class OnonboardingFooterRow extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container();
   }
-} */
+}
 
